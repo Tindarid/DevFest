f3c04c19575a7977b553d782fd7d8db324f24f49
Nursan Valeyev <tindarid@gmail.com>
2019-04-02 21:34:25 +0300

    Buttons fixed

--- app/src/main/java/ru/androidacademy/spb/devfest/ConferenceActivity.java
+++ app/src/main/java/ru/androidacademy/spb/devfest/ConferenceActivity.java
@@ -2,18 +2,26 @@ package ru.androidacademy.spb.devfest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.app.Activity;
 import android.content.Intent;
 
 public class ConferenceActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conference);
+        Button button = findViewById(R.id.all);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMain();
+            }
+        });
     }
 
-    public void startMain() {
+    private void startMain() {
         MainActivity.start(this);
     }
 
